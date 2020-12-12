c71dbed77f0a54752bcb628a9efaad4030370ad8
Stedman Halliday <stedman.rh@gmail.com>
2020-12-12 02:12:11 -0800

    gradient fn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+function bgGradient() {
+  fetch('https://raw.githubusercontent.com/itmeo/webgradients/master/gradients-parsed.json')
+    .then(response => response.json())
+    .then(data => {
+      const swatch = data[Math.floor(Math.random()*data.length)]
+      const deg = swatch.deg
+      let gradient = ""
+      for (const stop of swatch.gradient){
+        gradient += stop.color + " " + stop.pos + "%, "
+      }
+      gradient = gradient.substring(0, gradient.length-2)
+      const val = 'linear-gradient('+deg+'deg, '+ gradient+')'
+      console.log(val)
+    })
+}
+
+bgGradient() //get the value out of the function
+
+let AppStyle = {
+//   backgroundImage: bgGradient()
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={AppStyle}>
+      insert app here
     </div>
   );
 }
