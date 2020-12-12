e2a070a66cf62fbca2fd62cfa3ecfa81c26d812c
Stedman Halliday <stedman.rh@gmail.com>
2020-12-12 09:24:49 -0800

    gradient fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 
-function bgGradient() {
-  fetch('https://raw.githubusercontent.com/itmeo/webgradients/master/gradients-parsed.json')
-    .then(response => response.json())
-    .then(data => {
-      const swatch = data[Math.floor(Math.random()*data.length)]
-      const deg = swatch.deg
-      let gradient = ""
-      for (const stop of swatch.gradient){
-        gradient += stop.color + " " + stop.pos + "%, "
-      }
-      gradient = gradient.substring(0, gradient.length-2)
-      const val = 'linear-gradient('+deg+'deg, '+ gradient+')'
-      console.log(val)
-    })
+async function getGradient() {
+  const response = await fetch("https://raw.githubusercontent.com/itmeo/webgradients/master/gradients-parsed.json");
+  const gradients = await response.json();
+  // select random gradient from library and build style string
+  const swatch = gradients[Math.floor(Math.random()*gradients.length)];
+  const deg = swatch.deg;
+  let gradient = "";
+  for (const stop of swatch.gradient) {
+    gradient += stop.color + " " + stop.pos + "%, ";
+  }
+  gradient = gradient.substring(0, gradient.length-2);
+  const styleVal = 'linear-gradient('+deg+'deg, '+ gradient+')';
+  return styleVal;
 }
 
-bgGradient() //get the value out of the function
+// let AppStyle;
+// getGradient().then(result => {
+//   console.log(result);
+//   AppStyle = {
+//     backgroundImage: result
+//   }
+//   console.log(AppStyle);
+// })
 
-let AppStyle = {
-//   backgroundImage: bgGradient()
-}
+// console.log(AppStyle)
 
 function App() {
   return (
