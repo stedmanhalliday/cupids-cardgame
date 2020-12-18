1b96e7988275b6ca37772ac9a4650201c6d7b1d0
Stedman Halliday <stedman.rh@gmail.com>
2020-12-18 08:28:32 -0800

    next entry transition timing

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -5,6 +5,7 @@ class CardView extends React.Component {
     constructor(props) {
         super(props);
         this.mountStyle = this.mountStyle.bind(this);
+        this.nextRef = React.createRef();
         this.state = {
             style: {
                 opacity: 0
@@ -14,19 +15,27 @@ class CardView extends React.Component {
 
     mountStyle() {
         this.setState({
-            style: {}
+            style: {
+                transitionDelay: '500ms',
+                transitionDuration: '500ms'
+            }
         });
     }
 
     componentDidMount() {
         setTimeout(this.mountStyle, 10);
+        this.nextRef.current.ontransitionend = (e) => {
+            this.setState({
+                style: {}
+            });
+        };
     }
 
     render() {
         return(
             <div className="overlay">
                 <FlipCard pos={this.props.pos} promptGroup={this.props.promptGroup} prompt="If your life were a movie, what musician or band would create the soundtrack?" />
-                <div style={this.state.style} className="next fixed flex h-16 w-16 items-center justify-center top-1/2 transform translate-x-2/4 -translate-y-2/4 bg-white text-4xl text-red-400 font-sans font-extralight rounded-full shadow-2xl transition-opacity opacity-75 duration-500 cursor-pointer">&rarr;</div>
+                <div ref={this.nextRef} style={this.state.style} className="next fixed flex h-16 w-16 items-center justify-center top-1/2 transform translate-x-2/4 -translate-y-2/4 bg-white text-4xl text-red-400 font-sans font-extralight rounded-full shadow-2xl transition-opacity opacity-75 duration-300 cursor-pointer">&rarr;</div>
             </div>
         );
     }
