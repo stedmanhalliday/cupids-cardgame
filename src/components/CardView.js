d7924f9a78995f8064248e8ed64387c7f473e3ca
Stedman Halliday <stedman.rh@gmail.com>
2020-12-17 23:22:18 -0800

    next entrance transition

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -4,16 +4,29 @@ import { FlipCard } from './Card.js';
 class CardView extends React.Component {
     constructor(props) {
         super(props);
+        this.mountStyle = this.mountStyle.bind(this);
         this.state = {
-
+            style: {
+                opacity: 0
+            }
         };
+    }
+
+    mountStyle() {
+        this.setState({
+            style: {}
+        });
+    }
+
+    componentDidMount() {
+        setTimeout(this.mountStyle, 10);
     }
 
     render() {
         return(
             <div className="overlay">
                 <FlipCard pos={this.props.pos} promptGroup={this.props.promptGroup} prompt="If your life were a movie, what musician or band would create the soundtrack?" />
-                <div className="next fixed flex h-16 w-16 items-center justify-center top-1/2 transform translate-x-2/4 -translate-y-2/4 bg-white text-4xl text-red-400 font-sans font-extralight rounded-full shadow-2xl opacity-75 transition-opacity cursor-pointer">&rarr;</div>
+                <div style={this.state.style} className="next fixed flex h-16 w-16 items-center justify-center top-1/2 transform translate-x-2/4 -translate-y-2/4 bg-white text-4xl text-red-400 font-sans font-extralight rounded-full shadow-2xl transition-opacity opacity-75 duration-500 cursor-pointer">&rarr;</div>
             </div>
         );
     }
