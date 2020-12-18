745d83fbe92f9050acc6f23b7057314fbd9d83ad
Stedman Halliday <stedman.rh@gmail.com>
2020-12-18 09:51:27 -0800

    card refs to deck refs

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,11 @@ class Card extends React.Component {
     constructor(props){
         super(props);
         this.cardRef = React.createRef();
-        this.flipCard = this.flipCard.bind(this);
-    }
-
-    flipCard() {
-        const x = this.cardRef.current.getBoundingClientRect().left;
-        const y = this.cardRef.current.getBoundingClientRect().top;
-        const promptGroup = this.props.promptGroup;
-        this.props.flipCard(x, y, promptGroup);
     }
 
     render() {
         return (
-            <div ref={this.cardRef} className={this.props.flipped ? "Card flipped" : "Card"} style={this.props.style} onClick={this.flipCard}>
+            <div ref={this.cardRef} className={this.props.flipped ? "Card flipped" : "Card"} style={this.props.style} onClick={this.props.flipCard}>
                 <div className="faces relative h-full transition-all transform-gpu duration-500">
                     <div className="front bg-pink-50 px-6 pb-32 justify-center font-serif text-center text-red-400 uppercase tracking-widest">
                         <h2 className="prompt-group flex items-center h-24 p-4 border-4 border-double border-red-200 rounded-sm">{this.props.promptGroup}</h2>
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,14 +4,23 @@ import { Card } from './Card.js';
 class Deck extends React.Component {
     constructor(props){
         super(props);
+        this.flipCard = this.flipCard.bind(this);
+        this.DeckRef = React.createRef();
         this.state = {
         };
     }
 
+    flipCard() {
+        const x = this.DeckRef.current.getBoundingClientRect().left;
+        const y = this.DeckRef.current.getBoundingClientRect().top;
+        const promptGroup = this.props.promptGroup;
+        this.props.flipCard(x, y, promptGroup);
+    }
+
     render() {
         return (
-            <div className="Deck relative flex-none">
-                <Card flipCard={this.props.flipCard} promptGroup={this.props.promptGroup} />
+            <div ref={this.DeckRef} className="Deck relative flex-none">
+                <Card flipCard={this.flipCard} promptGroup={this.props.promptGroup} />
             </div>
         );
     }
