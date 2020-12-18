7768de2c5814ea8eb31f48ce94860289d8caaffe
Stedman Halliday <stedman.rh@gmail.com>
2020-12-18 09:37:30 -0800

    next fade

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -33,16 +33,23 @@ class CardView extends React.Component {
     }
 
     discard() {
-        //do stuff
-        //transition end
-        this.props.discard();
+        //animate card exit
+        this.setState({
+            style: {
+                transitionDuration: '500ms',
+                opacity: 0
+            }
+        });
+        this.nextRef.current.ontransitionend = (e) => { //replace with longer card transition
+            this.props.discard();
+        }
     }
 
     render() {
         return(
             <div className="overlay">
-                <FlipCard pos={this.props.pos} promptGroup={this.props.promptGroup} prompt="If your life were a movie, what musician or band would create the soundtrack?" />
                 <div ref={this.nextRef} onClick={this.discard} style={this.state.style} className="next fixed flex h-16 w-16 items-center justify-center top-1/2 transform translate-x-2/4 -translate-y-2/4 bg-white text-4xl text-red-400 font-sans font-extralight rounded-full shadow-2xl transition-opacity opacity-75 duration-300 cursor-pointer">&rarr;</div>
+                <FlipCard pos={this.props.pos} promptGroup={this.props.promptGroup} prompt="If your life were a movie, what musician or band would create the soundtrack?" />
             </div>
         );
     }
