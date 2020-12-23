203db0756cbc52bb1bb87c661dd009d1db0bf27f
Stedman Halliday <stedman.rh@gmail.com>
2020-12-23 15:03:02 -0800

    card rendering from deck data

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card } from "./Card.js";
 
 class Deck extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.flipCard = this.flipCard.bind(this);
         this.DeckRef = React.createRef();
@@ -11,21 +11,24 @@ class Deck extends React.Component {
     flipCard() {
         const x = this.DeckRef.current.getBoundingClientRect().left;
         const y = this.DeckRef.current.getBoundingClientRect().top;
-        const promptGroup = this.props.promptGroup;
-        this.props.flipCard(x, y, promptGroup);
+        this.props.flipCard(x, y, this.props.promptGroup);
     }
 
     render() {
-        let cards = [];
-        for(let n=(7-1); n>=0; n--) { //revise to reverse-traverse prompt array
-            if(n<7) {
-                const y = .25*n + "em";
-                const a = Math.pow(.6667, n);
-                const k = "scale(" + Math.pow(.9975, n) + ")";
-                const style = { top: y, opacity: a, transform: k };
-                cards.push(<Card key={n} style={style} flipCard={this.flipCard} promptGroup={this.props.promptGroup} />);
-            }
-        }
+        let deck = this.props.gameDecks.filter(deck =>
+            deck.promptGroup === this.props.promptGroup  //get correct deck
+        );
+        deck = deck[0];     //first obj el 
+        const cards = deck.prompts.map((card, n, hand) => {     //iterate through each card in each deck 
+            n = hand.length - (n + 1);    //reverse card iteration for stacking order
+            const y = .25 * n + "em";     //dY
+            const a = Math.pow(.6667, n);   //alpha
+            const k = "scale(" + Math.pow(.9975, n) + ")";      //scale
+            const style = { top: y, opacity: a, transform: k };     //style prop
+            return (
+                <Card key={card} style={style} flipCard={this.flipCard} promptGroup={deck.promptGroup} />
+            );
+        });
         return (
             <div ref={this.DeckRef} className="Deck relative flex-none">
                 {cards}
--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Deck from "./Deck.js";
+import { Card } from "./Card.js";
 
 class Decks extends React.Component {
 
     render() {
-        const decks = this.props.gameDecks.map(deck => {
+        const decks = this.props.gameDecks.map(deck => {    //iterate through each deck in game decks
+            // const cards = deck.prompts.map((card, n, hand) => {     //iterate through each card in each deck 
+            //     n = hand.length - (n+1);    //reverse card iteration for stacking order
+            //     const y = .25*n + "em";     //dY
+            //     const a = Math.pow(.6667, n);   //alpha
+            //     const k = "scale(" + Math.pow(.9975, n) + ")";      //scale
+            //     const style = { top: y, opacity: a, transform: k };     //style prop
+            //     return(
+            //         <Card key={card} style={style} flipCard={this.flipCard} promptGroup={deck.promptGroup} />
+            //     );
+            // });
             return(
-                <Deck key={deck.promptGroup} flipCard={this.props.flipCard} promptGroup={deck.promptGroup} />
+                <Deck key={deck.promptGroup} flipCard={this.props.flipCard} gameDecks={this.props.gameDecks} promptGroup={deck.promptGroup} />
             );
         });
 
