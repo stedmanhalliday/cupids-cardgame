7ea3e3f13f1a851afbfd7b3e7a11478c6d081249
Stedman Halliday <stedman.rh@gmail.com>
2020-12-23 12:07:38 -0800

    loading prompts library and stacking game decks

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Deck from "./Deck.js";
-import cards from "../data/cards.json";
+import promptsLibrary from "../data/prompts.json";
+
+let stackDecks = (promptsLibrary) => {
+    let gameDecks = [];
+    for (let n=0; n<5 && promptsLibrary.length; n++) {   //max 5 decks per game
+        const i = Math.floor(Math.random()*promptsLibrary.length);   //random deck index
+        const deck = promptsLibrary.splice(i, 1);    //pull random deck
+        gameDecks.push({     //name deck in game decks
+            "promptGroup": deck[0].promptGroup,
+            "prompts": []
+        });
+        const hand = deck[0].prompts;   //get cards from random deck
+        for (let m=0; m<7 && hand.length; m++) {    //max 7 cards per deck
+            const j = Math.floor(Math.random()*hand.length);    //random card index
+            const card = hand.splice(j, 1);     //pull random card
+            gameDecks[gameDecks.length-1].prompts.push(card);   //insert random card into game deck
+        }
+    }
+    return gameDecks;
+}
+
+let gameDecks = stackDecks(promptsLibrary);   //initialize game decks from card library
 
 class Decks extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            gameDecks: gameDecks
+        }
     }
 
     render() {
