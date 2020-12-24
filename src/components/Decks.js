f1a9e05944e184b1935e3656257c437e87d4238a
Stedman Halliday <stedman.rh@gmail.com>
2020-12-23 21:27:20 -0800

    conditional deck rendering

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -4,9 +4,14 @@ import Deck from "./Deck.js";
 class Decks extends React.Component {
 
     render() {
-        const decks = this.props.gameDecks.map(deck =>     //iterate through game decks
-            <Deck key={deck.promptGroup} flipCard={this.props.flipCard} gameDecks={this.props.gameDecks} promptGroup={deck.promptGroup} />
-        );
+        const decks = this.props.gameDecks.map(deck => {    //iterate through game decks
+            if(deck.prompts.length){    //only render decks with cards
+                return(
+                    <Deck key={deck.promptGroup} flipCard={this.props.flipCard} gameDecks={this.props.gameDecks} promptGroup={deck.promptGroup} />
+                );
+            }
+            return "";
+        });
 
         return (
             <section className="Decks flex overflow-scroll w-full h-full items-center space-x-6 transition-all">
