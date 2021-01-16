321676743a584a33da8ef44f27ecd74bb69416af
Stedman Halliday <stedman.rh@gmail.com>
2021-01-16 01:54:41 -0800

    mobile styles

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -35,7 +35,7 @@ class CardView extends React.Component {
     render() {
         return (
             <div className="overlay">
-                <button aria-label={"Next"} ref={this.discardRef} onClick={this.discard} style={this.state.style} className="discard fixed h-16 w-16 rounded-full transform translate-x-1/2 transition-opacity duration-500 focus:outline-none">
+                <button aria-label={"Next"} ref={this.discardRef} onClick={this.discard} style={this.state.style} className="discard fixed h-16 w-16 rounded-full transform translate-y-1/2 md:translate-x-1/2 md:translate-y-0 transition-opacity duration-500 focus:outline-none">
                     <span className="flex items-center justify-center w-full h-full rounded-full shadow-2xl text-white border-2 border-white font-sans font-extralight text-4xl transition-all duration-300">&rarr;</span>
                 </button>
                 <FlipCard pos={this.props.pos} discarded={this.state.discarded} promptGroup={this.props.promptGroup} prompt={this.props.prompt} />
